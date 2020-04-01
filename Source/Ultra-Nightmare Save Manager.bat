2343bbca174affbf92e8ad78351f23efb687716d
MrMendelli <mr_mendelli@yahoo.com>
2020-04-01 02:47:13 -0600

    Failsafe Added
    To be extra safe, the [Backup] is also backed up to the [Failsafe]. A precautionary measure in case something goes wrong. A new menu option has also been added to restore from the failsafe.

--- Source/Ultra-Nightmare Save Manager.bat
+++ Source/Ultra-Nightmare Save Manager.bat
@@ -12,7 +12,7 @@ set steamdir=%steamdir:/=\%
 :menu
 color 04
 title Ultra-Nightmare Save Manager
-mode con cols=60 lines=13
+mode con cols=60 lines=14
 cls
 echo     __________________________________________________
 echo    /                                                  \
@@ -20,27 +20,26 @@ echo    ^|                  BACKUP/RESTORE                  ^|
 echo    ^|__________________________________________________^|
 echo    ^| Backup ....................................... B ^|
 echo    ^| Restore ...................................... R ^|
+echo    ^| Failsafe ..................................... F ^|
 echo    ^| Exit ......................................... E ^|
 echo    \__________________________________________________/
 echo.
 set /p menu=" Backup or restore save data? "
 if /i "%menu%" equ "b" goto :backup
 if /i "%menu%" equ "r" goto :restore
+if /i "%menu%" equ "f" goto :failsafe
 if /i "%menu%" equ "e" goto :eof
 echo msgbox "Please enter 'b' or 'r' to proceed.", 0, "Error!" > "%TEMP%\Message.vbs"
 wscript "%TEMP%\Message.vbs" /wait
 goto :menu
 
-if exist ".\DOOM\[Backup]" goto :restore
-if not exist ".\DOOM\[Backup]" goto :backup
-
 :backup
 echo Backing files up...
+robocopy %savepath%\[Backup] %savepath%\[Failsafe] /s /is > nul
 robocopy %savepath%\base\savegame.user %savepath%\[Backup] /s /is > nul
 echo Backup complete!
 pause > nul
 goto :menu
-
 
 :restore
 echo Restoring save data...
@@ -48,6 +47,17 @@ taskkill /im DOOMx64.exe /f >nul 2>&1
 taskkill /im DOOMx64vk.exe /f >nul 2>&1
 robocopy %savepath%\[Backup] %savepath%\base\generated\temp\savegame.user /s /is >nul
 robocopy %savepath%\[Backup] %savepath%\base\savegame.user /s /is >nul
+echo Restoration complete!
+start "" "%steamdir%\Steam.exe" -applaunch 379720
+pause > nul
+goto :menu
+
+:failsafe
+echo Restoring failsafe data...
+taskkill /im DOOMx64.exe /f >nul 2>&1
+taskkill /im DOOMx64vk.exe /f >nul 2>&1
+robocopy %savepath%\[Failsafe] %savepath%\base\generated\temp\savegame.user /s /is >nul
+robocopy %savepath%\[Failsafe] %savepath%\base\savegame.user /s /is >nul
 echo Restoration complete!
 start "" "%steamdir%\Steam.exe" -applaunch 379720
 pause > nul
